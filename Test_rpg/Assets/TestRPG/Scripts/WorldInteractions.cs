e5a13ad9f6984b6d7fd97776faddabe107750906
Asterot13 <vitaliinovoselov@mail.ru>
2019-01-12 20:08:53 +0600

    New Scene
    Added new scene into the branch "develop"

---
+++ Test_rpg/Assets/TestRPG/Scripts/WorldInteractions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteractions : MonoBehaviour {
+
+    NavMeshAgent navMesh;
+
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            GetInteraction();
+        }
+    }
+
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObj = interactionInfo.collider.gameObject;
+            if (interactedObj.tag == "InteractableObj")
+            {
+                Debug.Log("Interactable object interacted");
+            }
+            else
+            {
+                navMesh.destination = interactionInfo.point;
+            }
+        }
+    }
+}
